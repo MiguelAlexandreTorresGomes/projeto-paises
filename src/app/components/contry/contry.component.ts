3dfcd903e2715de141524927483ccb6a9ff9939f
MiguelAlexandreTorresGomes <miguelatg74@gmail.com>
2025-09-19 13:57:55 -0300

    :sparkles: feat: filtros e api conectada

---
+++ src/app/components/contry/contry.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contry',
+  standalone: true,
+  imports: [],
+  templateUrl: './contry.component.html',
+  styleUrl: './contry.component.scss'
+})
+export class ContryComponent {
+
+}
--- src/app/components/country-list/country-list.component.ts
+++ src/app/components/country-list/country-list.component.ts
@@ -1,20 +1,81 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
+import { CountryService } from '../../services/country.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-country-list',
   standalone: true,
-  imports: [CommonModule, MatIcon],
+  imports: [CommonModule, MatIcon,FormsModule],
   templateUrl: './country-list.component.html',
   styleUrl: './country-list.component.scss'
 })
 export class CountryListComponent {
-  paises: any;
+  countries: any[] = [];
+  regions: string[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania', 'Antarctic'];
+  searchText: string = '';
   isDropdownOpen = false;
+  loading = true;
+
+  constructor(private countryService: CountryService) { }
+
+  ngOnInit(): void {
+    this.loadAllCountries();
+  }
+
+  loadAllCountries() {
+    this.loading = true;
+    this.countryService.getAllCountries().subscribe({
+      next: (data) => {
+        this.countries = data;
+        this.loading = false;
+      },
+      error: (err) => {
+        console.error('Erro ao carregar países', err);
+        this.loading = false;
+      }
+    });
+  }
 
   toggleDropdown() {
     this.isDropdownOpen = !this.isDropdownOpen;
   }
 
+  filterByRegion(region: string) {
+    if (!region) {
+      this.loadAllCountries();
+      return;
+    }
+    this.loading = true;
+    this.countryService.getByRegion(region).subscribe({
+      next: (data) => {
+        this.countries = data;
+        this.loading = false;
+      },
+      error: (err) => {
+        console.error('Erro ao filtrar países', err);
+        this.loading = false;
+      }
+    });
+  }
+  filterByCountry(name: string) {
+    name = name.trim();
+    if (!name) {
+      this.loadAllCountries();
+      return;
+    }
+    this.loading = true;
+    this.countryService.getCountryByName(name).subscribe({
+      next: (data) => {
+        this.countries = data;
+        this.loading = false;
+      },
+      error: (err) => {
+        console.error('Erro ao filtrar países', err);
+        this.countries = [];
+        this.loading = false;
+      }
+    });
+  }
 }
